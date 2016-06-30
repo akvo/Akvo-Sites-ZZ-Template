f2334447f5aeefdba1c7fb680f32548b29ac16ab
Samuel Thomas <samvthom16@gmail.com>
2016-06-30 17:48:59 +0530

    [#150] featured image in card for video

--- code/wp-content/themes/akvo-sites-new/partials/post-card.php
+++ code/wp-content/themes/akvo-sites-new/partials/post-card.php
@@ -6,11 +6,23 @@
         $post_type = get_post_type($post_id);
         
         $shortcode = '[akvo-card ';
-          	
-        $img = wp_get_attachment_url( get_post_thumbnail_id($post_id) );
+        
+        
+        $img = '';
+        if ($post_type == 'video') {
+    		$img = convertYoutubeImg(get_post_meta( get_the_ID(), '_video_extra_boxes_url', true ));
+    	}
+    	else{  	
+        	$img = wp_get_attachment_url( get_post_thumbnail_id($post_id) );
+        }
+        
+        
         if($img){
         	$shortcode .= 'img="'.$img.'" ';
         }
+        
+        
+        
         $shortcode .= 'title="'.get_the_title().'" date="'.get_the_date().'" content="'.get_the_excerpt().'" link="'.get_the_permalink().'" type="'.$post_type.'"]';
         			
         echo do_shortcode($shortcode);
