5ff12624439750f25daaf42718e36869b1059c0b
Samuel Thomas <samvthom16@gmail.com>
2016-06-30 15:07:33 +0530

    [#148] - infobar color of the card to be taken from the customize section

--- code/wp-content/themes/akvo-sites-new/lib/akvo-cards/main.php
+++ code/wp-content/themes/akvo-sites-new/lib/akvo-cards/main.php
@@ -281,3 +281,25 @@
 		return ob_get_clean();
 	}
 	add_shortcode( 'akvo-card', 'akvo_card' );
+	
+	function slugify($text){
+		// replace non letter or digits by -
+  		$text = preg_replace('~[^\pL\d]+~u', '-', $text);
+		// transliterate
+  		$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+  		// remove unwanted characters
+		$text = preg_replace('~[^-\w]+~', '', $text);
+
+		// trim
+		$text = trim($text, '-');
+
+		// remove duplicate -
+		$text = preg_replace('~-+~', '-', $text);
+
+		// lowercase
+		$text = strtolower($text);
+
+		if (empty($text)) {return 'n-a';}
+		return $text;
+	}
