d8c23e507b108f0c63cb4ad67ffd0994306e8bf8
Samuel Thomas <samvthom16@gmail.com>
2017-05-25 13:27:17 +0530

    [#284] - styles update for wvw

--- code/wp-content/themes/wvw/functions.php
+++ code/wp-content/themes/wvw/functions.php
@@ -3,6 +3,25 @@
 
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-   wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), '1.6.3');
+   wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), '1.9.1');
 
 }
+// Allow iframe tags within editor
+function allow_multisite_tags( $multisite_tags ){
+    $multisite_tags['iframe'] = array(
+        'src' => true,
+        'width' => true,
+        'height' => true,
+        'align' => true,
+        'class' => true,
+        'name' => true,
+        'id' => true,
+        'frameborder' => true,
+        'seamless' => true,
+        'srcdoc' => true,
+        'sandbox' => true,
+        'allowfullscreen' => true
+    );
+    return $multisite_tags;
+}
+add_filter('wp_kses_allowed_html','allow_multisite_tags', 1);
