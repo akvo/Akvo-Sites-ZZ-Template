4e1a2af2bc1211c52866c2cf7054cdc48ba229ac
Samuel Thomas <samvthom16@gmail.com>
2017-09-06 11:20:58 +0530

    [#332] - styling the support link

--- code/wp-content/themes/akvo-sites-new/functions.php
+++ code/wp-content/themes/akvo-sites-new/functions.php
@@ -30,10 +30,33 @@
 	
 	
 	add_action( 'admin_notices', function(){
-		_e('<div class="notice notice-warning is-dismissible"><p>');
-		_e( 'For Akvo Sites support, please check the <a target="_blank" href="http://sitessupport.akvo.org">documentation</a>.', 'akvo' );
-		_e('</p></div>');
+	?>
+		<div id="akvo-support-link" class="">
+			<a target="_blank" href="http://sitessupport.akvo.org">Help</a>
+		</div>
+		<style>
+			#akvo-support-link{
+				font-size: 16px;
+				background-color: #009900;
+				color: #fff;
+				position: fixed;
+				right: -10px;
+				padding: 8px 10px 10px;
+				z-index:230;
+				transform: rotate(270deg);
+				top: 200px;
+				border-top-left-radius: 5px;
+				border-top-right-radius: 5px;
+			}
+			#akvo-support-link a[href]{
+				color: inherit;
+				text-decoration: none;
+			}
+		</style>
+	<?php
 	} );
+	
+	
 	
 	
 	add_action( 'admin_init', function(){
