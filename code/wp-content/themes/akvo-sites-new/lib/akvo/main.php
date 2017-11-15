3a3da1fb7a23a1d90c9a7cd6aa551e3f3697ece0
Samuel Thomas <samvthom16@gmail.com>
2017-11-15 17:45:52 +0530

    [#332] - seperating code for admin dashboard

--- code/wp-content/themes/akvo-sites-new/lib/akvo/main.php
+++ code/wp-content/themes/akvo-sites-new/lib/akvo/main.php
@@ -3,7 +3,8 @@
 	$inc_files = array(
 		'class-akvo.php',
 		'class-akvo-customize.php',
-		'class-akvo-fonts.php'
+		'class-akvo-fonts.php',
+		'class-akvo-admin.php',
 	);
 	
 	foreach($inc_files as $inc_file){
