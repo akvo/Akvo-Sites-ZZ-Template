e28ad22cf1980e1bcce3ff5345cfabfc4b65b55f
Samuel Thomas <samvthom16@gmail.com>
2018-01-30 16:43:27 +0530

    [#359] - Hide sub menu items options in the user profile section

--- code/wp-content/themes/akvo-sites-new/lib/akvo/class-akvo-admin.php
+++ code/wp-content/themes/akvo-sites-new/lib/akvo/class-akvo-admin.php
@@ -3,6 +3,7 @@
 	class AKVO_ADMIN{
 		
 		var $menu;
+		var $submenu;
 		
 		function __construct(){
 			
@@ -34,10 +35,11 @@
 		}
 		
 		function hide_menu_items(){
-			global $menu;
+			global $menu, $submenu;
 				
 			/* SAVING THE OLD MENU FOR REUE IN THE USER FIELDS */
 			$this->menu = $menu;
+			$this->submenu = $submenu;
 				
 			$current_user = wp_get_current_user();
 				
@@ -46,9 +48,21 @@
 				
 			/* SELECTED MENU ITEMS FOR HIDING */
 			$menu_arr = is_array( get_user_meta( $current_user->ID, 'user_menu', true ) ) ? get_user_meta( $current_user->ID, 'user_menu', true ) : array();
+			$submenu_arr = is_array( get_user_meta( $current_user->ID, 'user_submenu', true ) ) ? get_user_meta( $current_user->ID, 'user_submenu', true ) : array();
 				
-			/* FINALLY HIDE */
+			/* FINALLY HIDE MENU PAGES */
 			foreach( $menu_arr as $menu_item ){ remove_menu_page( $menu_item ); }
+			
+			/* FINALLY HIDE SUBMENU PAGES */
+			foreach( $submenu_arr as $submenu_item ){ 
+				$submenu_item = explode(':', $submenu_item );
+				if( count( $submenu_item ) > 1 ){
+					remove_submenu_page( $submenu_item[0], $submenu_item[1] );
+				}
+				
+				
+			}
+			
 		}
 		
 		function contextual_help($old_help, $screen_id, $screen){
@@ -84,41 +98,70 @@
 		}
 		
 		function extra_user_profile_fields( $user ) { 
-			$exclude_menu = array( 'index.php', 'users.php' );
+			$exclude_menu = array( 'index.php', 'users.php', 'options-general.php' );
+			$exclude_submenu = array( 'index.php', 'profile.php', 'data-feed-options' );
+			
+			$labels_menu = array(
+				'index.php'				=> 'Dashboard',
+				'users.php'				=> 'Users',
+				'options-general.php'	=> 'Settings'
+			);
 			
 			$selected_menu = is_array( get_user_meta( $user->ID, 'user_menu', true ) ) ? get_user_meta( $user->ID, 'user_menu', true ) : array();
+			$selected_submenu = is_array( get_user_meta( $user->ID, 'user_submenu', true ) ) ? get_user_meta( $user->ID, 'user_submenu', true ) : array();
 			
-			global $submenu;
+			$submenu = $this->submenu;
 			
-			foreach( $exclude_menu as $menu_slug ){
-				// echo "<pre>";print_r( $submenu[ $menu_slug ] );echo "</pre>";
-			}
+			
 		?>
 			<h3><?php _e("Hide Menu Items"); ?></h3>
-
 			<table class="form-table">
-			<tr>
-				<td>
-					<ul>
-					<?php foreach( $this->menu as $menu_item ): ?>
-						<?php if( isset( $menu_item[0] ) && $menu_item[0] && isset( $menu_item[2] ) && $menu_item[2] && !in_array( $menu_item[2], $exclude_menu ) ):?>
-						<li style="display:inline-block;min-width: 200px;">
-							<label>
-								<input <?php if( in_array( $menu_item[2], $selected_menu ) )_e("checked='checked'");?> type="checkbox" name="user_menu[]" id="user-menu" value="<?php echo $menu_item[2]; ?>" />
-								<?php _e( $menu_item[0] );?>
-							</label>
-						</li>
-						&nbsp;&nbsp;
-						<?php endif;?>
-					<?php endforeach;?>
-					
-					<?php foreach( $exclude_menu as $menu_slug ):?>
-						
-					<?php endforeach; ?>
-					</ul>
-				</td>
-			</tr>
+				<!--tr>
+					<td>
+						<label>
+							<input type="checkbox" data-behaviour='toggle-menu-items' />
+							Select All
+						</label>
+					</td>
+				</tr-->
+				<!-- MAIN MENU ITEMS -->
+				<tr><td><ul>
+				<?php foreach( $this->menu as $menu_item ): ?>
+					<?php if( isset( $menu_item[0] ) && $menu_item[0] && isset( $menu_item[2] ) && $menu_item[2] && !in_array( $menu_item[2], $exclude_menu ) ):?>
+					<li class="hide-menu-items"><label>
+						<input <?php if( in_array( $menu_item[2], $selected_menu ) )_e("checked='checked'");?> type="checkbox" name="user_menu[]" id="user-menu" value="<?php echo $menu_item[2]; ?>" />
+						<?php _e( $menu_item[0] );?>
+					</label></li>
+					&nbsp;&nbsp;
+					<?php endif;?>
+				<?php endforeach;?>
+				</ul></td></tr>
+				<!-- END OF MAIN MENU ITEMS -->
+				<!-- SUB MENU ITEMS -->
+				<tr><td><ul>
+				<?php foreach( $exclude_menu as $menu_slug ):?>
+					<?php if( isset( $submenu[ $menu_slug ] ) && is_array( $submenu[ $menu_slug ] ) ):foreach( $submenu[ $menu_slug ] as $menu_item ):?>
+					<?php if( isset( $menu_item[0] ) && $menu_item[0] && isset( $menu_item[2] ) && $menu_item[2] && !in_array( $menu_item[2], $exclude_submenu ) ):?>
+					<li class="hide-menu-items"><label>
+						<?php $menu_val = $menu_slug.":".$menu_item[2];?>
+						<input <?php if( in_array( $menu_val, $selected_submenu ) )_e("checked='checked'");?> type="checkbox" name="user_submenu[]" id="user-submenu" value="<?php echo $menu_val; ?>" />
+						<?php _e( $labels_menu[ $menu_slug ]." : ".$menu_item[0] );?>
+					</label></li>
+					<?php endif;endforeach;endif;?>
+				<?php endforeach; ?>
+				</ul></td></tr>
+				<!-- END OF SUB MENU ITEMS -->
 			</table>
+			<style>
+				li.hide-menu-items{
+					display:inline-block;
+					min-width: 200px;
+				}
+				li.hide-menu-items .update-count, li.hide-menu-items .plugin-count, li.hide-menu-items .pending-count{ display:none; }
+			</style>
+			<script>
+				//alert( jQuery );
+			</script>
 		<?php }
 		
 		function save_extra_user_profile_fields( $user_id ) {
@@ -126,6 +169,7 @@
 				return false; 
 			}
 			update_user_meta( $user_id, 'user_menu', $_POST['user_menu'] );
+			update_user_meta( $user_id, 'user_submenu', $_POST['user_submenu'] );
 			
 		}
 		
