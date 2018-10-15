2aaa70df2387839042a05b49245113122fd7e8a5
Preeti <35522305+Preeti86@users.noreply.github.com>
2018-10-15 20:47:29 +0200

    -a -m “[#378] - policy page”

--- code/wp-content/themes/akvo-sites-new/lib/policy.php
+++ code/wp-content/themes/akvo-sites-new/lib/policy.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * Created by PhpStorm.
@@ -5,17 +6,14 @@
  * Date: 15/10/2018
  * Time: 9:45 AM
  */
-
 /**
  * Redirect the user to policy page,
  * if the user has not signed the policy terms.
  * And if signed direct the use to dashboard*/
 add_action('wp_login', 'check_meta_fields');
-
 function check_meta_fields($current_user)
 {
     $privacy_policy = array(
-
         'key' => 'privacy_policy',
         'value' => date('y-m-d'),
         'compare' => '='
@@ -25,10 +23,8 @@ function check_meta_fields($current_user)
     if (isset($privacy_policy) && !empty($privacy_policy)) {
         // this field was not set or was empty
         wp_redirect(admin_url('admin.php?page=options_page_slug'));  // or whatever success page
-
     }
 }
-
 /** Submenu and Landing page for PP & terms */
 class options_page
 {
@@ -45,24 +41,22 @@ class options_page
             'options_page_slug',
             array($this, 'settings_page')
         );
-
     }
     Public function settings_page()
     {
         ?>
-        <div class="wrap">
-            <div class="panel panel-success">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Privacy policy and terms</h3>
-                </div>
-                <p> We’re committed to ensuring that your privacy is protected and strictly follow the General Data
+                <div class="container">
+                    <div class="container" style="background-color:	#e0e5e5">
+                    <h3 >Privacy policy and terms</h3>
+                    <div class="container" style="background-color:white">
+                <p > We’re committed to ensuring that your privacy is protected and strictly follow the General Data
                     Protection Regulation (GDPR) rules.</p>
                 <div class="panel-body">
                     <form id="postform" name="postform" method="POST" action="#">
-                        <div class="form-group">
+                        <div class="container" style="background-color:white">
                             <fieldset>
                                 <label><input type="checkbox" required name="privacy_policy"
-                                              id="privacy_policy" value="<?php echo $_POST['privacy_policy']; ?>"/>Check
+                                              id="privacy_policy" class="form-radio" value="<?php echo $_POST['privacy_policy']; ?>"/>Check
                                     here
                                     to indicate that you
                                     have read and agree to the<a
@@ -71,6 +65,7 @@ class options_page
                                         terms</a>
                                     of Akvo foundation.</label>
                             </fieldset>
+
                             <br>
                             <div class="form-group">
                                 <button type="submit" id="submit" name="submit"
@@ -79,6 +74,10 @@ class options_page
                             </div>
                     </form>
                 </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
             </div>
         </div>
 
@@ -86,7 +85,6 @@ class options_page
         /**Hide the radio button and the submit once clicked*/
         $current_user = get_current_user_id();
         $privacy_policy = get_user_meta($current_user, 'privacy_policy', true);
-
         if ($privacy_policy) {
             ?>
             <style type="text/css" media="screen">
@@ -101,11 +99,9 @@ class options_page
         }
     }
 }
-
 /** Update user meta table
  * @return datetime to database table
  */
-
 $user_id = get_current_user_id();
 $current_date = date("Y-m-d H:i:s");
 if(isset($_POST['privacy_policy'])){
@@ -113,20 +109,16 @@ if(isset($_POST['privacy_policy'])){
     wp_redirect(admin_url('index.php'));  // or whatever success page
     exit;
 }
-
 /**Hide the admin menu and top bar*/
 add_action( 'admin_head', 'cn_admin_customize' );
 function cn_admin_customize($current_user){
     $privacy_policy = array(
-
         'key' => 'privacy_policy',
         'value' => date('Y-m-d H:i:s'),
         'compare' => '='
     );
-
     $current_user = get_current_user_id();
     $privacy_policy = get_user_meta($current_user, 'privacy_policy', true);
-
     if (!$privacy_policy) {
         ?>
         <style type="text/css" media="screen">
@@ -140,10 +132,30 @@ function cn_admin_customize($current_user){
         <?php
     }
 }
+/** CSS to the form */
+function admin_css() { ?>
+    <style type="text/css">
+        body {font-family: Arial, Helvetica, sans-serif;}
+        .container {
+            padding: 5px;
+            background-color: #f1f1f1;
+        }
+        h3{
+            color:#656d6d;
+        }
+        label{
+            font-weight: 700;
+        }
+
+    </style>
+<?php }
+add_action('admin_head', 'admin_css');
+
+
 
 // Function that outputs the contents of the dashboard widget
 function dashboard_widget_function( $post, $callback_args )
-{ ?><label> Here you read the <a href = "<?php echo site_url('/akvo-general-privacy-policy') ?>">privacy policy and terms </a>of Akvo Foundation.</label >
+{ ?><label> Here you can read the <a href = "<?php echo site_url('/akvo-general-privacy-policy') ?>">privacy policy and terms </a>of Akvo Foundation.</label >
     <?php
 }
 // Function used in the action hook
@@ -152,18 +164,5 @@ function add_dashboard_widgets() {
 }
 // Register the new dashboard widget with the 'wp_dashboard_setup' action
 add_action('wp_dashboard_setup', 'add_dashboard_widgets' );
-
-
-/**remove privacy policy from the settings menu, only the terms are accepted by the user
-add_action( 'admin_menu', 'nstrm_remove_admin_submenus', 999 );
-function nstrm_remove_admin_submenus()
-{
-$current_user = get_current_user_id();
-$privacy_policy = get_user_meta($current_user, 'privacy_policy', true);
-if ($privacy_policy) {
-remove_submenu_page('options-general.php', 'options_page_slug');
-
-}
-}*/
 new options_page;
 ?>
