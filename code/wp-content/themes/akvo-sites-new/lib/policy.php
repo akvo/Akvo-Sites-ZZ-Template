e745354ece2370bd8bdea8ea300a377c6ffd44bf
Samuel Thomas <samvthom16@gmail.com>
2018-10-23 09:25:18 +0530

    [#378] - refactor policy page

--- code/wp-content/themes/akvo-sites-new/lib/policy.php
+++ code/wp-content/themes/akvo-sites-new/lib/policy.php
@@ -1,36 +1,63 @@
 <?php
-/**
- * Redirect the user to policy page,
- * if the user has not signed the policy terms.
- * And if signed direct the use to dashboard*/
-add_action('wp_login', 'check_meta_fields');
-function check_meta_fields($current_user)
-{
-    $privacy_policy = array(
-        'key' => 'privacy_policy',
-        'value' => date('y-m-d'),
-        'compare' => '='
-    );
-    $current_user = get_current_user_id();
-    $privacy_policy = get_user_meta($current_user, 'privacy_policy', true);
-    if(!$privacy_policy) {
-        // this field was not set or was empty
-        wp_redirect(admin_url('admin.php?page=options_page_slug'));  // or whatever success page
-        exit;
-    }
-}
-/** Submenu and Landing page for PP & terms */
-class options_page
-{
-    public function __construct()
-    {
-        add_action('admin_menu', array($this, 'admin_menu'));
-    }
+/*
+* POLICY PAGE
+*/
 
-    function admin_menu()
-    {
-        $current_user = get_current_user_id();
-        $privacy_policy = get_user_meta($current_user, 'privacy_policy', true);
+class PRIVACY_POLICY_PAGE{
+	
+    public function __construct(){
+		
+		// ACTION HOOKS
+		add_action( 'wp_login', array( $this, 'wp_login' ) );
+		
+        add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		
+		add_action( 'admin_head', array( $this, 'admin_head' ) );
+		
+		add_action( 'admin_init', array( $this, 'admin_init' ) );
+    }
+	
+	/* GETTER AND SETTER FUNCTIONS */
+	function get_privacy_policy(){
+		$current_user = get_current_user_id();
+        return get_user_meta($current_user, 'privacy_policy', true);
+	}
+	
+	function set_privacy_policy( $value ){
+		update_user_meta( get_current_user_id(), 'privacy_policy', $value );
+	}
+	/* GETTER AND SETTER FUNCTIONS */
+	
+	/*
+	* Redirects the user to the policy page,
+	* if the user has not signed the policy terms.
+	* And if signed direct the use to dashboard
+	*/
+	function wp_login(){
+		$privacy_policy = $this->get_privacy_policy();
+		if( !$privacy_policy ) {
+			// this field was not set or was empty
+			wp_redirect(admin_url('admin.php?page=options_page_slug'));  // or whatever success page
+			exit;
+		}
+	}
+	
+	function admin_init(){
+		/* 	Update user meta table
+		* 	@return datetime to database table
+		*/
+		if(	isset(	$_POST['privacy_policy']	)	){
+			$current_date = date("Y-m-d H:i:s");
+			$this->set_privacy_policy( $current_date );
+			wp_redirect( admin_url( 'index.php' ) );  // or whatever success page
+			exit;
+		}
+	}
+	
+    function admin_menu(){
+        
+        $privacy_policy = $this->get_privacy_policy();
+		
         if (!$privacy_policy) {
             add_options_page(
                 'Privacy_policy',
@@ -42,86 +69,53 @@ class options_page
         }
     }
 
-    function settings_page()
-    {
-            ?>
-            <div class="container">
-                <div class="container" style="background-color:	#e0e5e5">
-                    <h3>Privacy policy and terms</h3>
-                    <div class="container2" style="background-color:white">
-                        <p> We’re committed to ensuring that your privacy is protected and strictly follow the General
-                            Data
-                            Protection Regulation (GDPR) rules.</p>
-                        <div class="panel-body">
-                            <form id="postform" name="postform" method="POST" action="#">
-                                <div class="container3" style="background-color:white">
-                                    <fieldset>
-                                        <label><input type="checkbox" required name="privacy_policy"
-                                                      id="privacy_policy" class="form-radio"
-                                                      value="<?php echo $_POST['privacy_policy']; ?>"/>Check
-                                            here
-                                            to indicate that you
-                                            have read and agree to the<a
-                                                    href="<?php echo site_url('/akvo-general-terms-and-conditions') ?>">
-                                                privacy-policy and
-                                                terms</a>
-                                            of Akvo foundation.</label>
-                                    </fieldset>
+    function settings_page(){
+	?>
+	<div class="container">
+		<div class="container" style="background-color:	#e0e5e5">
+			<h3>Privacy policy and terms</h3>
+			<div class="container2" style="background-color:white">
+				<p> We’re committed to ensuring that your privacy is protected and strictly follow the General Data Protection Regulation (GDPR) rules.</p>
+				<div class="panel-body">
+					<form id="postform" name="postform" method="POST" action="">
+						<div class="container3" style="background-color:white">
+							<fieldset>
+								<label>
+									<input type="checkbox" required name="privacy_policy" id="privacy_policy" class="form-radio" value="<?php echo $_POST['privacy_policy']; ?>"/>
+									Check here to indicate that you have read and agree to the
+									<a href="<?php echo site_url('akvo-general-terms-and-conditions') ?>">privacy-policy and terms</a>
+									of Akvo foundation.
+								</label>
+							</fieldset>
+							<br>
+							<div class="form-group">
+								<button type="submit" id="submit" name="submit" class="button button-primary button-large">Accept</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	<?php
+		
+		/**Hide the radio button and the submit once clicked*/
+		$privacy_policy = $this->get_privacy_policy();
+		if( $privacy_policy ):
+	?>
+		<style type="text/css" media="screen">
+			#privacy_policy { display: none; }
+			#submit { display: none; }
+		</style>
+	<?php
+		endif;
 
-                                    <br>
-                                    <div class="form-group">
-                                        <button type="submit" id="submit" name="submit"
-                                                class="button button-primary button-large">Accept
-                                        </button>
-                                    </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <?php
-            /**Hide the radio button and the submit once clicked*/
-            $current_user = get_current_user_id();
-            $privacy_policy = get_user_meta($current_user, 'privacy_policy', true);
-            if ($privacy_policy) {
-                ?>
-                <style type="text/css" media="screen">
-                    #privacy_policy {
-                        display: none;
-                    }
-
-                    #submit {
-                        display: none;
-                    }
-
-                </style>
-                <?php
-            }
-
-        }
-}
-/** Update user meta table
- * @return datetime to database table
- */
-$user_id = get_current_user_id();
-$current_date = date("Y-m-d H:i:s");
-if(isset($_POST['privacy_policy'])){
-    update_user_meta($user_id,'privacy_policy',$current_date);
-    wp_redirect(admin_url('index.php'));  // or whatever success page
-    exit;
-}
-/**Hide the admin menu and top bar*/
-add_action( 'admin_head', 'cn_admin_customize' );
-function cn_admin_customize($current_user){
-    $privacy_policy = array(
-        'key' => 'privacy_policy',
-        'value' => date('Y-m-d H:i:s'),
-        'compare' => '='
-    );
-    $current_user = get_current_user_id();
-    $privacy_policy = get_user_meta($current_user, 'privacy_policy', true);
-    if (!$privacy_policy) {
+	}
+	
+	
+	function admin_head() {
+		/** Hide the admin menu and top bar */
+		$privacy_policy = $this->get_privacy_policy();
+		if( !$privacy_policy ) {
         ?>
         <style type="text/css" media="screen">
             #wp-toolbar {
@@ -136,48 +130,35 @@ function cn_admin_customize($current_user){
 
         </style>
         <?php
-    }
+		}
+		/** CSS to the form */
+	?>
+		<style type="text/css">
+			body {font-family: Arial, Helvetica, sans-serif;}
+			.container { padding: 8px; width: 933px;}
+			.container2 { padding: 5px; width: 920px;}
+			.container3 { padding: 5px; width: 900px; }
+			h3{ color:#656d6d; padding: 8px;}
+			label{ font-weight: 700;}
+
+		</style>
+	<?php 
+	}
+	
 }
-/** CSS to the form */
-function admin_css() { ?>
-    <style type="text/css">
-        body {font-family: Arial, Helvetica, sans-serif;}
-        .container {
-            padding: 8px;
-            width: 933px;
 
-        }
-        .container2 {
-            padding: 5px;
-            width: 920px;
-        }
-        .container3 {
-            padding: 5px;
-            width: 900px;
-        }
-        h3{
-            color:#656d6d;
-            padding: 8px;
-        }
-        label{
-            font-weight: 700;
-        }
 
-    </style>
-<?php }
-add_action('admin_head', 'admin_css');
+new PRIVACY_POLICY_PAGE;
 
 // Function that outputs the contents of the dashboard widget
-function dashboard_widget_function( $post, $callback_args )
-{ ?><label> Here you can read the <a href = "<?php echo site_url('akvo-general-terms-and-conditions') ?>">privacy policy and terms </a>of Akvo Foundation.</label >
-    <?php
-}
-// Function used in the action hook
-function add_dashboard_widgets() {
-    wp_add_dashboard_widget('dashboard_widget', 'Privacy policy and terms', 'dashboard_widget_function');
-}
-// Register the new dashboard widget with the 'wp_dashboard_setup' action
-add_action('wp_dashboard_setup', 'add_dashboard_widgets' );
-
-new options_page;
+function dashboard_widget_function( $post, $callback_args ){ 
 ?>
+	<label> Here you can read the <a href = "<?php echo site_url('akvo-general-terms-and-conditions') ?>">privacy policy and terms </a>of Akvo Foundation.</label >
+<?php
+}
+
+// Register the new dashboard widget with the 'wp_dashboard_setup' action
+add_action('wp_dashboard_setup', function(){
+	wp_add_dashboard_widget('dashboard_widget', 'Privacy policy and terms', 'dashboard_widget_function');
+} );
+
