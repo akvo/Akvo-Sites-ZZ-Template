27bc0004a3cc4972e00ba831fba094a86d4a428f
unknown <samvthom16@gmail.com>
2019-05-13 15:30:39 +0530

    #481 - pull specific field from RSR project API

--- code/wp-content/themes/akvo-sites-2019/lib/akvo-cards/class-akvo-rsr.php
+++ code/wp-content/themes/akvo-sites-2019/lib/akvo-cards/class-akvo-rsr.php
@@ -1,86 +1,107 @@
 <?php
 
 	class AKVO_RSR{
-		
-		function get_json_data( $data_feed_id ){
-			
-			//$data = do_shortcode('[data_feed name="'.$data_feed_id.'"]'); 		/* Dependancy on the Data Feed Plugin */
-			
-			//return json_decode( str_replace('&quot;', '"', $data) );
-			
-			return $this->get_data_feed_response( $data_feed_id );
-				
+
+		function __construct(){
+			add_shortcode( 'akvo_rsr_project_field', array( $this, 'display_project_field' ) );
 		}
-		
+
+		function display_project_field( $atts ){
+			$atts = shortcode_atts( array(
+				'feed'	=> 'Sample',
+				'field'	=> 'project_plan'
+			), $atts, 'akvo_rsr_project_field' );
+
+			$json_data = $this->get_data_feed_response( $atts['feed'] );
+
+			$field = $atts['field'];
+
+			if( isset( $json_data->$field ) ){
+				return $json_data->$field;
+			}
+		}
+
+		function get_json_data( $data_feed_id ){
+
+			$shortcode_str = '[data_feed name="'.$data_feed_id.'"]';
+
+			$data = do_shortcode( $shortcode_str ); 		/* Dependancy on the Data Feed Plugin */
+
+			return json_decode( str_replace('&quot;', '"', $data) );
+
+			//return $this->get_data_feed_response( $data_feed_id );
+
+		}
+
 		/* GET DATA FEEDS FROM THE DATABASE */
 		function get_data_feeds(){
 			global $wpdb;
-			
+
 			$data_feeds = array();
-			
+
 			$rows = $wpdb->get_results( 'SELECT df_name FROM ' . $wpdb->prefix . 'data_feeds' );
-			
+
 			foreach( $rows as $row ){
 				$data_feeds[ $row->df_name ] = $row->df_name;
 			}
-			
+
 			return $data_feeds;
-			
+
 		}
-		
+
 		/* GET DATA FEED URL FROM ID */
 		function get_data_feed_url( $data_feed_id ){
-			
+
 			global $wpdb;
-			
+
 			$rows = $wpdb->get_results( "SELECT df_url FROM " . $wpdb->prefix . "data_feeds WHERE df_name='" . $data_feed_id . "' LIMIT 0,1;" );
-			
+
 			foreach( $rows as $row ){
 				return $row->df_url;
 			}
-			
+
 			return false;
 		}
-		
+
 		function get_data_feed_response( $data_feed_id ){
-			
+
 			$json_key = 'df'.$data_feed_id;
 			$_json_expiration = 60 * 5; // 5 minutes
 			$key = $json_key . md5($json_key );
-			
+
 			$data = array();
-			
+
 			if ( ! ( $data = get_transient($key) ) ) {
-				
+
 				$url = $this->get_data_feed_url( $data_feed_id );
-				
+
 				$request = wp_remote_get( $url );
-				
+
 				if( ! is_wp_error( $request ) ) {
 					$body = wp_remote_retrieve_body( $request );
-					
+
 					// MORE OF YOUR CODE HERE
 					$data = json_decode( $body );
 
 					// IF IT IS NEW, SET THE TRANSIENT FOR NEXT TIME
 					set_transient($key, $data, $_json_expiration);
 				}
-				
+
 			}
-			
+
 			return $data;
-			
+
 		}
-		
+
 		function get_feed_api( $url ){
-			
+
 			$body = false;
-			
+
 			try{
 				$request = wp_remote_get( $url );
-				
+
 				//print_r( $request );
-					
+
 				if( ! is_wp_error( $request ) ) {
 					$body = wp_remote_retrieve_body( $request );
 				}
@@ -89,7 +110,7 @@
 			}
 			return $body;
 		}
-		
+
 		function get_base_url( $akvo_card_options ){
 			$base_url = 'http://rsr.akvo.org';
 			if($akvo_card_options && array_key_exists('akvoapp', $akvo_card_options)){
@@ -97,18 +118,18 @@
 			}
 			return $base_url;
 		}
-		
+
 		function parse_rsr( $rsr_obj, $akvo_card_options, $akvo_date_format, $type ){
-			
+
 			$data = array( 'type-text'	=> 'RSR Project' );
 			if( $type == 'update' ){
 				$data[ 'type-text' ] = 'RSR Updates';
 			}
-			
+
 			$base_url = self::get_base_url( $akvo_card_options );								// GET BASE URL
-			
+
 			/* PARSING JSON OBJECT OF RSR PROJECT */
-			
+
 			/* COMMON RSR OBJECTS */
 			if( isset( $rsr_obj->title ) ){
 				$data['title'] = $rsr_obj->title;												/* rsr object title */
@@ -120,7 +141,7 @@
 				$data['link'] = $base_url.$rsr_obj->absolute_url;								/* rsr absolute url */
 			}
 			/* COMMON RSR OBJECTS */
-			
+
 			/* RSR OBJECT CONTENT */
 			if( $type == 'project' && isset( $rsr_obj->project_plan_summary ) ){
 				$data['content'] = truncate( $rsr_obj->project_plan_summary, 130 );				/* rsr project summary */
@@ -129,7 +150,7 @@
 				$data['content'] = truncate( $rsr_obj->text, 130 );								/* rsr updates text */
 			}
 			/* RSR OBJECT CONTENT */
-			
+
 			/* RSR OBJECT IMAGE */
 			if( $type == 'project' && isset( $rsr_obj->current_image ) ){
 				$data['img'] = $base_url.$rsr_obj->current_image;
@@ -139,17 +160,17 @@
 					$data['img'] = $base_url.$rsr_obj->photo->original;
 				}
 				else{
-					$data['img'] = $base_url.$rsr_obj->photo;	
+					$data['img'] = $base_url.$rsr_obj->photo;
 				}
 			}
 			/* RSR OBJECT IMAGE */
-			
+
 			return $data;
-			
+
 		}
-		
-		
+
+
 	}
-	
+
 	global $akvo_rsr;
 	$akvo_rsr = new AKVO_RSR;
